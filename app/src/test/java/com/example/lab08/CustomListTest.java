c77f2e723d04691f108e6545215ab6cb4e033acd
Harjot Singh <Harjotmatharu16@gmail.com>
2025-10-28 17:29:32 -0600

    implemented hasCity inside of CustomList.java

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,15 +1,16 @@
 package com.example.lab08;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+public class CustomListTest{
 
-/**
- * Hello
- */
-
-public class CustomListTest {
-
-    hasCity()
-
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Cagary", "AB");
+        list.addCity(calgary);
+        Assertions.assertTrue(list.hasCity(calgary));
+    }
 }
